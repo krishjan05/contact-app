17c3517362704647f41718a2b68f3eaf77a2ca7d
Krishna Kanhaiya <krishjan05@gmail.com>
2021-09-06 07:25:10 -0400

    added some more fields

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function ContactApp() {
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [bio, setBio] = useState("");
+  const [userType, setUserType] = useState("");
   const [contactList, setContactList] = useState([]);
 
   const handleSubmit = (event) => {
@@ -12,6 +14,8 @@ function ContactApp() {
     const contact = {
       name: `${name}`,
       phoneNumber: `${phoneNumber}`,
+      bio: `${bio}`,
+      userType: `${userType}`
     }
     contactList.push(contact);
     const sortedArray = contactList.sort((a, b) => {
@@ -29,9 +33,16 @@ function ContactApp() {
       return 0;
     });
     setContactList(sortedArray => [...sortedArray]);
+    clearForm();
+  }
+
+  const clearForm = () => {
     setName("");
     setPhoneNumber("");
+    setBio("");
+    setUserType("user");
   }
+
 
 
   return (
@@ -46,6 +57,23 @@ function ContactApp() {
           <label>Phone Number</label>
           <input className="form-control" type="text" name="phoneNumber" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
         </div>
+        <div className="mb-3">
+          <label className="form-label">Profile Image</label>
+          <input className="form-control" type="file" onChange={e => console.log(e.target.files)} />
+        </div>
+        <div className="form-group">
+          <label>User Type</label>
+          <select className="form-control" value={userType} onChange={e => setUserType(e.target.value)}>
+            <option>Choose a user type</option>
+            <option value="admin">Admin</option>
+            <option value="supplier">Supplier</option>
+            <option value="user">User</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Bio</label>
+          <textarea className="form-control" row="10" value={bio} onChange={e => setBio(e.target.value)}></textarea>
+        </div>
         <button className="btn btn-primary mt-3">Add</button>
       </form>
       {
@@ -58,6 +86,8 @@ function ContactApp() {
                   <td scope="col">Sr No</td>
                   <td scope="col">Name</td>
                   <td scope="col">Phone Number</td>
+                  <td scope="col">Bio</td>
+                  <td scope="col">User Type</td>
                 </tr>
               </thead>
               <tbody>
@@ -66,6 +96,8 @@ function ContactApp() {
                     <td>{index + 1}</td>
                     <td>{contact.name}</td>
                     <td>{contact.phoneNumber}</td>
+                    <td>{contact.bio}</td>
+                    <td>{contact.userType}</td>
                   </tr>
                 )}
               </tbody>
